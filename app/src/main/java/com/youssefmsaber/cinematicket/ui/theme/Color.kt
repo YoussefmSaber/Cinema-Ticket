75cad37c112450b165a7b8053213af87264912e7
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-06-11 15:34:09 +0300

    Initial commit

---
+++ app/src/main/java/com/youssefmsaber/cinematicket/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.youssefmsaber.cinematicket.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val OrangeButton = Color (0xFFFF5624)
+
+val LightBrown = Color(0xFFA2988B)
+val DarkBrown = Color (0xFF958E84)
+
+val SeatTakenColor = Color(0xFF404040)
+val SecondaryTextColor = Color(0xFFC2C5CD)
---
+++ app/src/main/java/com/youssefmsaber/cinematicket/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package com.youssefmsaber.cinematicket.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.youssefmsaber.cinematicket.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_extralight, FontWeight.ExtraLight),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_semibold, FontWeight.SemiBold),
+)
